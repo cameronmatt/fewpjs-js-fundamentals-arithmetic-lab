e4bc092a7f83f6679e1bec87593524052f0bbc16
Matthew <cameron.matt@outlook.com>
2021-03-04 10:19:58 +1100

    done fundamentals-arithmeti

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 100;
+const num4 = 96;
+
+
+let multiply = (num1 * num2);
+let random = Number.isInteger(Math.random()) + 1;
+let mod = (num3 - num4);
+let max = ((num3 / num2) - num1 + 1);
